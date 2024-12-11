eef65cd8bdc8426756189ce70639a432cc4553bf
CallumB04 <callumburgoyne04@gmail.com>
2024-12-11 18:50:57 +0000

    feat: create basic navbar structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 function Navbar() {
 
     return (
-        <nav>
-            <ul>
-                
-            </ul>
+        <nav className="absolute top-0 left-0 w-screen h-16 px-24 bg-transparent flex justify-between items-center">
+            <h2 className="font-semibold font-ssp text-3xl tracking-wide">PlayRates</h2>
+            <div className="font-ssp h-full flex gap-7 items-center">
+                <p>Browse Games</p>
+                <input type="text" placeholder="Search for game..."/>
+                <p>My Account</p>
+            </div>
         </nav>
     );
 }
