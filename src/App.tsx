260ffe91470c4d95cad4d2f5e28cf2b5c00dfede
CallumB04 <callumburgoyne04@gmail.com>
2024-12-11 17:18:47 +0000

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path='/' element={<></>} />
+      </Routes>
+    </Router>
   )
 }
 
