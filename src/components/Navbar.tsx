3133d39ad570d219b1ec010187ddb01edbdbf301
CallumB04 <callumburgoyne04@gmail.com>
2024-12-12 16:06:06 +0000

    feat: link navbar text to destinations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
 
     return (
         <nav className="absolute top-0 left-0 z-50 w-screen h-16 px-24 bg-transparent flex justify-between items-center">
             <h2 className="font-semibold font-ssp text-gray-100 text-3xl tracking-wide">PlayRates</h2>
             <div className="font-ssp font-normal text-gray-100 h-full flex gap-4 items-center">
-                <p className="p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">My Account</p>
-                <p className="p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">Browse Games</p>
+                <Link to="/account">
+                    <p className="p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
+                        My Account
+                    </p>
+                </Link>
+                <Link to="/library">
+                    <p className="p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
+                        Browse Games
+                    </p>
+                </Link>
                 <input 
                     type="text" 
                     placeholder="Search for game..."
