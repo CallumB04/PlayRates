e060da6f221330ba5ee92bcf70ec15d838d8d2c0
CallumB04 <callumburgoyne04@gmail.com>
2024-12-12 17:24:49 +0000

    feat: add dropdown when hovering my account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,28 +3,33 @@ import { Link } from "react-router-dom";
 function Navbar() {
 
     return (
-        <nav className="absolute top-0 left-0 z-50 w-screen h-16 md:px-6 xl:px-16 bg-transparent 
+        <nav className="absolute top-0 left-0 z-50 w-screen h-16 md:px-6 xl:px-16 bg-[#232323] 
                         flex justify-center md:justify-between items-center">
             <h2 className="font-semibold font-ssp text-gray-100 text-4xl tracking-wide">PlayRates</h2>
             <div className="hidden md:flex font-ssp font-normal text-gray-100 h-full gap-4 items-center">
                 <Link to="/account">
-                    <p className="flex gap-1 items-center p-2 transition duration-50 
-                                  hover:cursor-pointer hover:text-purple-600">
-                        My Account
-                        <i className="fa fa-chevron-down text-sm"></i>
-                    </p>
+                    <span className="relative group flex gap-1 items-center p-2 hover:cursor-pointer">
+                        <p className="group-hover:text-purple-600 transition duration-75">My Account</p>
+                        <i className="fa fa-chevron-down text-sm group-hover:text-purple-600 transition duration-75"></i>
+
+                        <div className="absolute hidden hover:block hover:cursor-default group-hover:block mx-auto w-72 h-52 top-10 pt-3">
+                            <div className="w-full h-full bg-neutral-700 rounded-b-md">
+
+                            </div>
+                        </div>
+                    </span>
                 </Link>
                 <Link to="/library">
-                    <p className="block p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
+                    <p className="block p-2 transition duration-75 hover:cursor-pointer hover:text-purple-600">
                         Browse Games
                     </p>
                 </Link>
                 <input 
                     type="text" 
                     placeholder="Search for game..."
-                    className="block w-60 px-2 py-1.5 bg-neutral-700 rounded"
+                    className="block w-60 px-2 py-1.5 bg-neutral-700 rounded focus:outline-none"
                 />
-            </div>
+            </div>            
         </nav>
     );
 }
