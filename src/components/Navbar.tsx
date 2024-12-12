f88bd2c0342872337f0e88c94923041ec8c4e31e
CallumB04 <callumburgoyne04@gmail.com>
2024-12-12 16:29:10 +0000

    feat: add responsiveness to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
 
     return (
-        <nav className="absolute top-0 left-0 z-50 w-screen h-16 px-16 bg-transparent flex justify-between items-center">
+        <nav className="absolute top-0 left-0 z-50 w-screen h-16 md:px-6 lg:px-16 bg-transparent flex justify-center md:justify-between items-center">
             <h2 className="font-semibold font-ssp text-gray-100 text-4xl tracking-wide">PlayRates</h2>
-            <div className="font-ssp font-normal text-gray-100 h-full flex gap-4 items-center">
+            <div className="hidden md:flex font-ssp font-normal text-gray-100 h-full gap-4 items-center">
                 <Link to="/account">
                     <p className="flex gap-1 items-center p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
                         My Account
@@ -13,14 +13,14 @@ function Navbar() {
                     </p>
                 </Link>
                 <Link to="/library">
-                    <p className="p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
+                    <p className="block p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
                         Browse Games
                     </p>
                 </Link>
                 <input 
                     type="text" 
                     placeholder="Search for game..."
-                    className="w-60 px-2 py-1.5 bg-neutral-700 rounded"
+                    className="block w-60 px-2 py-1.5 bg-neutral-700 rounded"
                 />
             </div>
         </nav>
