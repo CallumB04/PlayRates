1dc51a22293ae18c4df59ee078b7b10980e64ff2
CallumB04 <callumburgoyne04@gmail.com>
2024-12-12 16:30:50 +0000

    feat: change breakpoint for increased padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
 
     return (
-        <nav className="absolute top-0 left-0 z-50 w-screen h-16 md:px-6 lg:px-16 bg-transparent flex justify-center md:justify-between items-center">
+        <nav className="absolute top-0 left-0 z-50 w-screen h-16 md:px-6 xl:px-16 bg-transparent 
+                        flex justify-center md:justify-between items-center">
             <h2 className="font-semibold font-ssp text-gray-100 text-4xl tracking-wide">PlayRates</h2>
             <div className="hidden md:flex font-ssp font-normal text-gray-100 h-full gap-4 items-center">
                 <Link to="/account">
-                    <p className="flex gap-1 items-center p-2 transition duration-50 hover:cursor-pointer hover:text-purple-600">
+                    <p className="flex gap-1 items-center p-2 transition duration-50 
+                                  hover:cursor-pointer hover:text-purple-600">
                         My Account
                         <i className="fa fa-chevron-down text-sm"></i>
                     </p>
