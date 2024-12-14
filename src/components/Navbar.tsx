c1bfddd00d1168631b005141cd847f3351a23e04
CallumB04 <callumburgoyne04@gmail.com>
2024-12-14 14:08:00 +0000

    chore: add colors to config file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,11 +28,11 @@ function Navbar() {
 
 
     return (
-        <nav className="absolute top-0 left-0 z-50 w-screen h-16 px-6 xl:px-8 bg-[#232323] 
+        <nav className="absolute top-0 left-0 z-50 w-screen h-16 px-6 xl:px-8 bg-navbarColor 
                         flex justify-center md:justify-between items-center">
             {/* Site logo, links to home page */}
             <Link to="/" onClick={closePhoneDropdown}>
-                <h2 className="font-semibold font-ssp text-gray-100 text-3xl md:text-4xl tracking-wide">
+                <h2 className="font-semibold font-ssp text-textColor text-3xl md:text-4xl tracking-wide">
                     PlayRates
                 </h2>
             </Link>
@@ -41,28 +41,28 @@ function Navbar() {
             <div ref={phoneDropdownBtn} 
                  className="md:hidden absolute right-5 flex flex-col gap-1 group p-[6px]
                             hover:cursor-pointer">
-                <div className="w-[22px] h-[2px] bg-gray-100 group-hover:bg-purple-600 transition-colors"></div>
-                <div className="w-[22px] h-[2px] bg-gray-100 group-hover:bg-purple-600 transition-colors"></div>
-                <div className="w-[22px] h-[2px] bg-gray-100 group-hover:bg-purple-600 transition-colors"></div>
+                <div className="w-[22px] h-[2px] bg-textColor group-hover:bg-highlightPurple transition-colors"></div>
+                <div className="w-[22px] h-[2px] bg-textColor group-hover:bg-highlightPurple transition-colors"></div>
+                <div className="w-[22px] h-[2px] bg-textColor group-hover:bg-highlightPurple transition-colors"></div>
             </div>
 
             {/* Phone dropdown menu */}
             <div ref={phoneDropdown} 
-                 className="bg-[#333] absolute top-16 right-0 w-0 h-screen rounded-l-md
+                 className="bg-dropdownColor absolute top-16 right-0 w-0 h-screen rounded-l-md
                             overflow-y-scroll overflow-x-hidden transition-width duration-300
                             delay-50 ease-in-out md:hidden">
 
             </div>
 
             {/* Navbar items wrapper */}
-            <div className="hidden md:flex font-ssp font-normal text-gray-100 h-full gap-4 items-center">
+            <div className="hidden md:flex font-ssp font-normal text-textColor h-full gap-4 items-center">
                 {/* My Account and Dropdown wrapper */}
                 <span className="relative group">
                     {/* My Account text */}
                     <span className="flex gap-1 items-center p-2 hover:cursor-pointer 
                                                    group-hover:pb-5 group-hover:mt-3">
-                        <p className="group-hover:text-purple-600 transition duration-75">My Account</p>
-                        <i className="fa fa-chevron-down text-sm group-hover:text-purple-600 transition duration-75"></i>
+                        <p className="group-hover:text-highlightPurple transition duration-75">My Account</p>
+                        <i className="fa fa-chevron-down text-sm group-hover:text-highlightPurple transition duration-75"></i>
                     </span>
 
                     {/* Dropdown menu wrapper */}
@@ -70,7 +70,7 @@ function Navbar() {
                                     group-hover:h-[280px] transition-height duration-[400ms] delay-50 ease-in-out
                                     hover:block hover:cursor-default group-hover:block">
                         {/* Dropdown menu content */}
-                        <div className="w-full h-full bg-[#333] rounded-b-md overflow-hidden
+                        <div className="w-full h-full bg-dropdownColor rounded-b-md overflow-hidden
                                         flex items-center flex-col gap-[2px] font-normal">
                             
                             {/* All dropdown links currently link to a template account for testing
@@ -78,35 +78,35 @@ function Navbar() {
                                 account's pages. E.G: /user/<username>/played */}
 
                             <Link className={`${dropdownItemStyles} mt-3 pl-[9px]`} to="/user/callum">
-                                <i className="fa-solid fa-user text-purple-600"></i>
+                                <i className="fa-solid fa-user text-highlightPurple"></i>
                                 <p>My Profile</p>
                             </Link>
                             <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 
-                                            pt-[1px] my-1 w-11/12 bg-gray-100"></span>
+                                            pt-[1px] my-1 w-11/12 bg-textColor"></span>
                             <Link className={`${dropdownItemStyles}`} to="/user/callum/played">
-                                <i className="fa-regular fa-check-circle text-purple-600"></i>
+                                <i className="fa-regular fa-check-circle text-highlightPurple"></i>
                                 <p>Played</p>
                             </Link>
                             <Link className={`${dropdownItemStyles}`} to="/user/callum/playing">
-                                <i className="fa-regular fa-play-circle text-purple-600"></i>
+                                <i className="fa-regular fa-play-circle text-highlightPurple"></i>
                                 <p>Playing</p>
                             </Link>
                             <Link className={`${dropdownItemStyles}`} to="/user/callum/backlog">
-                                <i className="fa-regular fa-calendar-plus text-purple-600"></i>
+                                <i className="fa-regular fa-calendar-plus text-highlightPurple"></i>
                                 <p>Backlog</p>
                             </Link>
                             <Link className={`${dropdownItemStyles}`} to="/user/callum/wishlist">
-                                <i className="fa-solid fa-heart text-purple-600"></i>
+                                <i className="fa-solid fa-heart text-highlightPurple"></i>
                                 <p>Wishlist</p>
                             </Link>
                             <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 
-                                            pt-[1px] my-1 w-11/12 bg-gray-100"></span>
+                                            pt-[1px] my-1 w-11/12 bg-textColor"></span>
                             <Link className={`${dropdownItemStyles}`} to="/settings">
-                                <i className="fa-solid fa-cog text-purple-600"></i>
+                                <i className="fa-solid fa-cog text-highlightPurple"></i>
                                 <p>Settings</p>
                             </Link>
                             <Link className={`${dropdownItemStyles}`} to="">
-                                <i className="fa-solid fa-right-from-bracket text-purple-600"></i>
+                                <i className="fa-solid fa-right-from-bracket text-highlightPurple"></i>
                                 <p>Sign Out</p>
                             </Link>
 
@@ -115,7 +115,7 @@ function Navbar() {
                 </span>
                 {/* Games library link text */}
                 <Link to="/library">
-                    <p className="block p-2 transition duration-75 hover:cursor-pointer hover:text-purple-600">
+                    <p className="block p-2 transition duration-75 hover:cursor-pointer hover:text-highlightPurple">
                         Browse Games
                     </p>
                 </Link>
@@ -128,7 +128,7 @@ function Navbar() {
                     />
                     <i className="fas fa-magnifying-glass absolute 
                                   transform -translate-y-1/2 top-1/2 right-1 p-2
-                                  text-neutral-400 hover:text-purple-600 hover:cursor-pointer
+                                  text-neutral-400 hover:text-highlightPurple hover:cursor-pointer
                                   transition-colors"></i>
                 </span>
             </div>            
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ export default {
         height: 'height',
         width: 'width',
       },
+      colors: {
+        navbarColor: "#232323",
+        dropdownColor: "#333333",
+        textColor: "#f3f4f6",
+        highlightPurple: "#9333ea",
+      }
     },
   },
   plugins: [],
