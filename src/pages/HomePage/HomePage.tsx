623f3a757dd1000da94aa8dc5fd16dedae3b44ed
CallumB04 <callumburgoyne04@gmail.com>
2024-12-15 20:17:33 +0000

    feat: add title and slogan

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
 function HomePage() {
 
     return (
-        <main>
-            
+        <main className="px-4 sm:px-10 md:px-16 xl:px-28 pt-24 md:pt-36">
+            <h1 className="font-ssp font-bold text-7xl md:text-8xl lg:text-[7rem] text-textColor">PlayRates</h1>
+            <h2 className="font-ssp font-semibold text-2xl lg:text-3xl text-[#cacaca]
+                           ml-1 mt-3 md:mt-5">All of your games in one place...</h2>
         </main>
     );
 }
