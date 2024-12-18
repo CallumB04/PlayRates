12b544fcc18eeb797f4fa11300ecc686ecaa73f0
CallumB04 <callumburgoyne04@gmail.com>
2024-12-18 16:58:17 +0000

    chore: add query client for data fetching and caching

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import './styles/fonts.css';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a Query Client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
