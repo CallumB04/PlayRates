23b68708b48d637d38434e4698ee94119d6b9455
CallumB04 <callumburgoyne04@gmail.com>
2024-12-18 16:56:54 +0000

    feat: fetch user account before loading site

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,40 @@
 import './styles/App.css';
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage/HomePage';
-import { useEffect, useState } from 'react';
-
-// Structure for the game data
-// Will add further data for each game in future
-export interface Game {
-  id: number;
-  title: string;
-}
-
-// Structure for a user's log of a game
-// Will fetch Game using the gameId
-export interface GameLog {
-  id: number;
-  gameId: number;
-  status: "played" | "playing" | "backlog" | "wishlist";
-  rating?: number;
-}
-
-// Structure for game list in user account
-export interface GameList {
-  played: GameLog[];
-  playing: GameLog[];
-  backlog: GameLog[];
-  wishlist: GameLog[];
-}
-
-// Structure for user account
-// Currently uses json for easy development, will implement secure database in future
-export interface UserAccount {
-  id: number;
-  accountName: string;
-  email: string;
-  password: string;
-  games: GameList;
-}
+import { fetchUserById, UserAccount } from "./api";
 
 
 function App() {
 
-  const [gamesData, setGamesData] = useState<Game[] | null>(null);
-  const [userData, setUserData] = useState<UserAccount | null>(null);
+    const [user, setUser] = useState<UserAccount | undefined>(undefined);
 
-  /* In the future, I will use a secure database to hold user and game data,
-     however currently I am using local json files to simplify development */
+    useEffect(() => {
+        const loadUser = async () => {
+            const fetchedUser = await fetchUserById(0);
 
-  // fetch game data from json
-  useEffect(() => {
-    const fetchGameData = async () => {
-      try {
-        const response = await fetch("/PlayRates/data/games.json");
-        const gameData = await response.json();
-        setGamesData(gameData);
-      } catch (error) {
-        console.error(error);
-      }
+            if (fetchedUser) { 
+                setUser(fetchedUser); 
+            }
+        };
+
+        loadUser();
+    }, []);
+
+    if (!user) { 
+      return <p className="text-center text-5xl text-textColor pt-20">Loading...</p>;
     }
-
-    fetchGameData();
-  }, [])
-
-  // fetch user data from json
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch("/PlayRates/data/user.json");
-        const userData = await response.json();
-        setUserData(userData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchUserData();
-  }, []);
-
-
-  return (
-    <Router basename="/PlayRates">
-      <Navbar userData={userData}/>
-      <Routes>
-        <Route path='/' element={<HomePage gamesData={gamesData} userData={userData}/>} />
-      </Routes>
-    </Router>
-  )
+    
+    
+    return (
+        <Router basename="/PlayRates">
+            <Navbar user={user}/>
+            <Routes>
+                <Route path='/' element={<HomePage user={user}/>} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
