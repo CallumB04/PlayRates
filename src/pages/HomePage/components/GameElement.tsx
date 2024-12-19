996dd987e85fa659243ad3b0e08c677ffcfd2524
CallumB04 <callumburgoyne04@gmail.com>
2024-12-19 23:17:36 +0000

    feat: render images for games

--- src/pages/HomePage/components/GameElement.tsx
+++ src/pages/HomePage/components/GameElement.tsx
@@ -8,8 +8,15 @@ const GameElement: React.FC<GameElementProps> = ({ game }) => {
 
     return (
         <div key={game.id} 
-            className="w-[170px] md:w-[200px] 2xl:w-[230px] h-72 bg-red-500 rounded-md">
-            {game.title}
+            className="w-[170px] md:w-[200px] 2xl:w-[220px] h-60 md:h-72 bg-[#0e0e0e] rounded-md relative
+                       flex justify-center items-center">
+            {/* Text displayed if image can't be loaded */}
+            <p className="text-center text-lg text-textColor break-words whitespace-normal">
+                {game.title}
+            </p>
+            <img className = "absolute top-0 left-0 object-cover w-full h-full rounded-md"
+                 src={`./PlayRates/assets/game-covers/${game.id}.png`} 
+                 onError={(e) => e.currentTarget.style.display = "none"}/>
         </div>
     )
 };
