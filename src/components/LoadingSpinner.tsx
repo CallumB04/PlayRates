43efb199ef7070b8fa84abcbd0b129b2f23fbd6b
CallumB04 <callumburgoyne04@gmail.com>
2024-12-20 00:36:42 +0000

    fix: ensure possible spinner sizes are purged

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,8 +4,11 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({size}) => {
     
+    // ensure possible spinner sizes arent purged on deployment
+    // size options: size-3 size-4 size-5 size-6 size-7 size-8
+
     return (
-        <div className={`size-${size} border-2 border-textColor 
+        <div className={`size-${size-1} md:size-${size} border-2 border-textColor 
                        border-t-highlightPurple rounded-full animate-spin`}>
         </div>
     )
