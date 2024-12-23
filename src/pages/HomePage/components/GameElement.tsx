ebc128424ef28ef90fdee3191b21b9dcf56b6c95
CallumB04 <callumburgoyne04@gmail.com>
2024-12-23 18:30:11 +0000

    feat: improve text size on game cards

--- src/pages/HomePage/components/GameElement.tsx
+++ src/pages/HomePage/components/GameElement.tsx
@@ -15,10 +15,10 @@ const GameElement: React.FC<GameElementProps> = ({ game }) => {
             className="w-[31%] md:w-[15%] aspect-[3/4] 
                      bg-[#0e0e0e] rounded-md relative flex justify-center items-center group">
             {/* Text displayed if image can't be loaded */}
-            <p className={`text-center text-xl text-textColor break-words whitespace-normal
-                          group-hover:z-20 font-semibold
+            <p className={`text-center text-base sm:text-xl md:text-base lg:text-xl text-textColor 
+                          break-words whitespace-normal group-hover:z-20 font-semibold
                           ${imgLoaded ? "opacity-0" : "opacity-100"} group-hover:opacity-100 
-                          transition-opacity duration-300`}>
+                          transition-opacity duration-300 px-1`}>
                 {game.title}
             </p>
             <img className="absolute top-0 left-0 object-cover w-full h-full rounded-md
@@ -29,8 +29,8 @@ const GameElement: React.FC<GameElementProps> = ({ game }) => {
                 Currently no functionality, will allow users to add games
                 to their account from the home page. */}
             <i className="fa-solid fa-plus text-textColor absolute bottom-3 mx-auto w-1/3 h-[14%]
-                          bg-[#ffffff22] rounded-md text-lg sm:text-xl md:text-base lg:text-lg xl:text-xl opacity-0 
-                          group-hover:opacity-100 transition-all duration-300 hover:bg-[#ffffff33] flex justify-center
+                          bg-[#ffffff22] rounded-md hidden sm:flex text-lg md:text-sm lg:text-lg xl:text-xl opacity-0 
+                          group-hover:opacity-100 transition-all duration-300 hover:bg-[#ffffff33] justify-center
                           items-center"></i>
         </Link>
     )
