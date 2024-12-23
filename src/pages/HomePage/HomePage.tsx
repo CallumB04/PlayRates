45d2ce22261ae86b43ba1ae4c051803c8da79cd9
CallumB04 <callumburgoyne04@gmail.com>
2024-12-23 17:41:29 +0000

    chore: change name for recently released games section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -98,8 +98,8 @@ const HomePage = () => {
                                       .slice(0, 6)
             } loading={gamesLoading} error={gamesError}/>
 
-            {/* Most recent games (by order of release date) */}
-            <h2 className={gameSectionTitleStyles}>Most Recent</h2>
+            {/* Newly released games */}
+            <h2 className={gameSectionTitleStyles}>New Releases</h2>
             <GameSection games={games?.sort((a, b) => Date.parse(b.releaseDate) - Date.parse(a.releaseDate))
                                       .slice(0, 6)
             } loading={gamesLoading} error={gamesError}/>
