394fff3c9e81743392c31179384512d459a84ae6
CallumB04 <callumburgoyne04@gmail.com>
2025-01-23 16:05:38 +0000

    feat: improve notification design

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,27 +1,37 @@
 interface NotificationProps {
     text: string;
-    type: "success" | "error" | "loading";
+    type: "success" | "error";
 }
 
-// function to return tailwind css color to match the notification type
-const typeToColor = (type: string) => {
+// function to return text color, bg color and icon to match the notification type
+const typeToValues = (
+    type: string
+): { textColor: string; bgColor: string; iconName: string } | undefined => {
     switch (type) {
         case "success":
-            return "bg-green-500";
+            return {
+                textColor: "text-green-500",
+                bgColor: "bg-green-100",
+                iconName: "circle-check",
+            };
         case "error":
-            return "bg-red-500";
-        case "loading":
-            return "bg-highlight-primary";
+            return {
+                textColor: "text-red-500",
+                bgColor: "bg-red-100",
+                iconName: "circle-exclamation",
+            };
     }
 };
 
 const Notification: React.FC<NotificationProps> = ({ text, type }) => {
+    const { textColor, bgColor, iconName } = typeToValues(type)!;
+
     return (
         <p
-            className={`text-text-primary ${typeToColor(type)} notification-fadeout fixed left-1/2 top-20 z-50 mx-auto h-max w-max -translate-x-1/2 rounded-md px-4 py-2 font-lexend text-lg`}
+            className={`${textColor} ${bgColor} notification-fadeout fixed left-1/2 top-20 z-50 mx-auto flex h-max w-max -translate-x-1/2 flex-row items-center gap-x-2 rounded-md px-4 py-2 font-lexend text-lg`}
         >
-            <span className="font-semibold uppercase">{type}: </span>
-            {text}
+            <i className={`fa-solid fa-${iconName}`}></i>
+            <p>{text}</p>
         </p>
     );
 };
