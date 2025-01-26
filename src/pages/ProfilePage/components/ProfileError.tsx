2562f53350fe2b8f08d68486b775cf7d54277e00
Cal <callumburgoyne04@gmail.com>
2025-01-26 04:11:22 +0000

    feat: add buttons on user fetch error

--- src/pages/ProfilePage/components/ProfileError.tsx
+++ src/pages/ProfilePage/components/ProfileError.tsx
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
+import { useUser } from "../../../App";
 
 // Displayed if no target user in URL or failed to fetch user from API
 const ProfileError = () => {
+    const user = useUser();
+
     return (
-        <div className="mx-auto mt-6 flex w-max flex-col items-center gap-y-5">
-            <h1 className="font-lexend text-xl text-text-primary">
+        <div className="flex h-[70vh] w-full flex-col items-center justify-center gap-y-5 px-4">
+            <h1 className="text-center font-lexend text-xl text-text-primary">
                 There was an error when fetching this user...
             </h1>
-            <Link to="/">
-                <p className="button-primary w-max">Return to home page</p>
-            </Link>
+            <div className="flex w-full flex-col items-center gap-3 sm:w-max sm:flex-row">
+                <Link to="/" className="button-primary w-full sm:w-max">
+                    Return to home page
+                </Link>
+                {user ? (
+                    <Link
+                        to={`/user/${user.username}`}
+                        className="button-secondary w-full sm:w-max"
+                    >
+                        Go to Profile
+                    </Link>
+                ) : (
+                    <></>
+                )}
+            </div>
         </div>
     );
 };
