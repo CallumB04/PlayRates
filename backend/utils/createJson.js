c0db1e02eb4f379ff50c66e88be58d9ef5c202d9
Cal <callumburgoyne04@gmail.com>
2025-02-21 10:47:39 +0000

    feat: individualise file creation checks

--- backend/utils/createJson.js
+++ backend/utils/createJson.js
@@ -2,9 +2,17 @@ const fs = require("fs");
 const usersFilePath = "./data/users.json";
 const gamesFilePath = "./data/games.json";
 
-// creating json files with empty arrays if files dont exist
+// create data folder if doesnt exist
+if (!fs.existsSync("data")) {
+    fs.mkdirSync("data");
+}
+
+// create users.json with empty array if file doesnt exist
 if (!fs.existsSync(usersFilePath)) {
-    fs.mkdirSync("data"); // create data folder
     fs.writeFileSync(usersFilePath, "[]", "utf8");
+}
+
+// create games.json with empty array if file doesnt exist
+if (!fs.existsSync(gamesFilePath)) {
     fs.writeFileSync(gamesFilePath, "[]", "utf8");
 }
