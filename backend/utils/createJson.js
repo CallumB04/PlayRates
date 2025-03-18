605aa55145b8d6b6761d64a64fd1ef79213879b1
CallumB04 <callumburgoyne04@gmail.com>
2025-03-19 01:32:38 +0200

    chore: create new json file if missing

--- backend/utils/createJson.js
+++ backend/utils/createJson.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const usersFilePath = "./data/users.json";
 const gamesFilePath = "./data/games.json";
 const friendsFilePath = "./data/friends.json";
+const gameLogsFilePath = "./data/gamelogs.json";
 
 // create data folder if doesnt exist
 if (!fs.existsSync("data")) {
@@ -18,7 +19,12 @@ if (!fs.existsSync(gamesFilePath)) {
     fs.writeFileSync(gamesFilePath, "[]", "utf8");
 }
 
-// create friends.json with empty array if file doesnt exist
+// create friends.json with empty object if file doesnt exist
 if (!fs.existsSync(friendsFilePath)) {
     fs.writeFileSync(friendsFilePath, "{}", "utf8");
+}
+
+// create gamelogs.json with empty object if file doesnt exist
+if (!fs.existsSync(gameLogsFilePath)) {
+    fs.writeFileSync(gameLogsFilePath, "{}", "utf8");
 }
