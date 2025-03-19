4e6925863b39475c10bc7d72751977005cdce31f
CallumB04 <callumburgoyne04@gmail.com>
2025-03-19 16:37:06 +0200

    feat: add new fields to game log

--- frontend/src/api/gamelogs.ts
+++ frontend/src/api/gamelogs.ts
@@ -6,7 +6,12 @@ axios.defaults.baseURL = `http://${API_IP ? API_IP : "localhost"}:3000`; // sett
 export interface GameLog {
     id: number; // corresponding game id
     status: string; // played, playing, backlog, wishlist
-    rating?: number; // optional rating, in range of 1-10
+    rating?: number; // range of 1-10, step of 0.25 (e.g: 6.25, 8.75, 3.5, 5.0)
+    hoursPlayed?: number; // to 1 decimal point
+    hoursToBeat?: number; // to 1 decimal point
+    startDate?: string; // stored in ISO 8601 format for easy Date object conversion
+    finishDate?: string; // stored in ISO 8601 format for easy Date object conversion
+    platform?: string; // Steam, Xbox Game pass, etc
 }
 
 export const fetchGameLogs = async () => {
