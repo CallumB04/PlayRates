c2ffcc17e85d9071d299dcf7f008c21c38348d33
CallumB04 <callumburgoyne04@gmail.com>
2025-03-19 09:51:21 +0200

    feat: add hover text to profile games

--- frontend/src/pages/ProfilePage/components/GameElement.tsx
+++ frontend/src/pages/ProfilePage/components/GameElement.tsx
@@ -23,11 +23,18 @@ const GameElement: React.FC<GameElementProps> = ({ gameLog }) => {
 
     if (game) {
         return (
-            <div className="game-cover w-1/3 p-1 sm:w-1/4 md:w-[14%] xl:w-[11%]">
+            <div className="game-cover group relative w-1/3 p-1 sm:w-1/4 md:w-[14%] xl:w-[11%]">
                 <img
                     className="h-full w-full rounded-md object-cover"
                     src={`/PlayRates/assets/game-covers/${game.id}.png`}
                 />
+                <div className="absolute left-0 top-0 h-full w-full p-1">
+                    <div className="flex h-full w-full items-center justify-center rounded-md transition-colors duration-200 group-hover:bg-[#0e0e0eaa]">
+                        <p className="line-clamp-3 break-words px-1 text-center font-lexend text-lg text-text-primary opacity-0 transition-opacity duration-200 group-hover:opacity-100 sm:text-base lg:text-sm 2xl:text-base">
+                            {game.title}
+                        </p>
+                    </div>
+                </div>
             </div>
         );
     }
