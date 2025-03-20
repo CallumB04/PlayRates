cffae5b570a4036de797811833e2f9efcc6694e1
CallumB04 <callumburgoyne04@gmail.com>
2025-03-20 20:45:01 +0200

    feat: improve layout in popup

--- frontend/src/pages/ProfilePage/components/ViewGameLogPopup.tsx
+++ frontend/src/pages/ProfilePage/components/ViewGameLogPopup.tsx
@@ -55,85 +55,93 @@ const ViewGameLogPopup: React.FC<ViewGameLogPopupProps> = ({
                                 src={`/PlayRates/assets/game-covers/${game?.id}.png`}
                             />
                         </div>
-                        <div className="flex flex-col gap-1 pl-4 text-left">
-                            <p className="text-text-primary">
-                                Status:{" "}
-                                <span
-                                    className={`font-light ${getColorFromGameStatus(gamelog!.status)?.bg} ${getColorFromGameStatus(gamelog!.status)?.text} rounded-full px-1.5 py-0.5`}
-                                >
-                                    {gamelog?.status[0].toUpperCase()}
-                                    {gamelog?.status.slice(1)}
-                                </span>
-                            </p>
-                            {gamelog?.platform ? (
+                        <div className="flex flex-grow justify-between">
+                            <div className="flex flex-col gap-1 pl-4 text-left">
                                 <p className="text-text-primary">
-                                    Platform:{" "}
-                                    <span className="font-extralight">
-                                        {gamelog.platform}
-                                        {/* TODO: ADD LOGO instead of / next to text */}
+                                    Status:{" "}
+                                    <span
+                                        className={`font-light ${getColorFromGameStatus(gamelog!.status)?.bg} ${getColorFromGameStatus(gamelog!.status)?.text} rounded-full px-1.5 py-0.5`}
+                                    >
+                                        {gamelog?.status[0].toUpperCase()}
+                                        {gamelog?.status.slice(1)}
                                     </span>
                                 </p>
-                            ) : (
-                                <></>
-                            )}
-                            {gamelog?.startDate ? (
-                                <p className="text-text-primary">
-                                    Started:{" "}
-                                    <span className="font-extralight">
-                                        {gamelog.startDate}
-                                    </span>
-                                </p>
-                            ) : (
-                                <></>
-                            )}
-                            {gamelog?.startDate ? (
-                                <p className="text-text-primary">
-                                    Finished:{" "}
-                                    <span className="font-extralight">
-                                        {gamelog.finishDate || "N/A"}
-                                    </span>
-                                </p>
-                            ) : (
-                                <></>
-                            )}
-                            {gamelog?.hoursPlayed ? (
-                                <p className="text-text-primary">
-                                    Time Played:{" "}
-                                    <span className="font-extralight">
-                                        {gamelog.hoursPlayed}
-                                        <span className="text-sm"> hours</span>
-                                    </span>
-                                </p>
-                            ) : (
-                                <></>
-                            )}
-                            {gamelog?.hoursToBeat ? (
-                                <p className="text-text-primary">
-                                    Completed in:{" "}
-                                    <span className="font-extralight">
-                                        {gamelog.hoursToBeat}
-                                        <span className="text-sm"> hours</span>
-                                    </span>
-                                </p>
-                            ) : (
-                                <></>
-                            )}
-                        </div>
+                                {gamelog?.platform ? (
+                                    <p className="text-text-primary">
+                                        Platform:{" "}
+                                        <span className="font-extralight">
+                                            {gamelog.platform}
+                                            {/* TODO: ADD LOGO instead of / next to text */}
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <></>
+                                )}
+                                {gamelog?.startDate ? (
+                                    <p className="text-text-primary">
+                                        Started:{" "}
+                                        <span className="font-extralight">
+                                            {gamelog.startDate}
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <></>
+                                )}
+                                {gamelog?.startDate ? (
+                                    <p className="text-text-primary">
+                                        Finished:{" "}
+                                        <span className="font-extralight">
+                                            {gamelog.finishDate || "N/A"}
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <></>
+                                )}
+                                {gamelog?.hoursPlayed ? (
+                                    <p className="text-text-primary">
+                                        Time Played:{" "}
+                                        <span className="font-extralight">
+                                            {gamelog.hoursPlayed}
+                                            <span className="text-sm">
+                                                {" "}
+                                                hours
+                                            </span>
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <></>
+                                )}
+                                {gamelog?.hoursToBeat ? (
+                                    <p className="text-text-primary">
+                                        Completed in:{" "}
+                                        <span className="font-extralight">
+                                            {gamelog.hoursToBeat}
+                                            <span className="text-sm">
+                                                {" "}
+                                                hours
+                                            </span>
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <></>
+                                )}
+                            </div>
 
-                        <div className="flex flex-grow flex-col gap-1 pr-4">
-                            <span className="flex items-center justify-end gap-2 text-lg">
-                                <i className="fas fa-trophy text-yellow-300"></i>
-                                <p className="font-extralight text-text-primary">
-                                    {gamelog?.achievementsCompleted || 0}/
-                                    {gamelog?.achievementsTotal || "?"}
-                                </p>
-                            </span>
-                            <span className="flex items-center justify-end gap-2 text-lg">
-                                <i className="fas fa-star text-highlight-hover"></i>
-                                <p className="font-extralight text-text-primary">
-                                    {gamelog?.rating || "?"}/10
-                                </p>
-                            </span>
+                            <div className="flex flex-col gap-1 pr-4">
+                                <span className="flex items-center justify-start gap-2 text-lg">
+                                    <i className="fas fa-trophy text-yellow-300"></i>
+                                    <p className="font-extralight text-text-primary">
+                                        {gamelog?.achievementsCompleted || 0}/
+                                        {gamelog?.achievementsTotal || "?"}
+                                    </p>
+                                </span>
+                                <span className="flex items-center justify-start gap-2 text-lg">
+                                    <i className="fas fa-star text-highlight-hover"></i>
+                                    <p className="font-extralight text-text-primary">
+                                        {gamelog?.rating || "?"}/10
+                                    </p>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
