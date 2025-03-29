48d718792ff78e911992115f4067e678f2c116d8
CallumB04 <callumburgoyne04@gmail.com>
2025-03-29 16:38:12 +0200

    feat: map all games onto page

--- frontend/src/pages/LibraryPage/LibraryPage.tsx
+++ frontend/src/pages/LibraryPage/LibraryPage.tsx
@@ -1,11 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchGameLogsByUserID, fetchGames, Game, GameLog } from "../../api";
+import { useUser } from "../../App";
+import GameElement from "./components/GameElement";
+
 const LibraryPage = () => {
+    const currentUser = useUser();
+
+    // fetching all games from API
+    const {
+        data: games,
+        error: gamesError,
+        isLoading: gamesLoading,
+    } = useQuery<Game[] | undefined>({
+        queryKey: ["games"],
+        queryFn: () => fetchGames(),
+    });
+
+    // fetching current users game logs from API, if logged in
+    const {
+        data: currentUserGameLogs,
+        error: currentUserGameLogsError,
+        isLoading: currentUserGameLogsLoading,
+    } = useQuery<GameLog[] | undefined>({
+        queryKey: ["currentUserGameLogs", currentUser?.id],
+        queryFn: () => fetchGameLogsByUserID(currentUser!.id),
+        enabled: !!currentUser,
+    });
+
     return (
-        <section className="flex w-full flex-col gap-5 lg:h-[85vh] lg:flex-row">
-            <aside className="card h-full w-full lg:hidden xl:flex xl:w-1/4 2xl:w-1/5">
+        <section className="flex w-full gap-4">
+            {/* Filters Menu (for larger screens) */}
+            <aside className="card hidden h-[85vh] w-80 lg:flex">
                 <h2 className="card-header-text">Filters</h2>
             </aside>
-            <div className="card w-full xl:w-3/4 2xl:w-4/5">
-                <h2 className="card-header-text">Game Library</h2>
+            {/* Games */}
+            <div className="flex h-max w-full flex-wrap gap-1">
+                {games?.map((game) => {
+                    return (
+                        <GameElement
+                            key={game.id}
+                            game={game}
+                            userLoggedIn={currentUser ? true : false}
+                            userHasLog={
+                                currentUserGameLogs?.some(
+                                    (log) => log.id === game.id
+                                )
+                                    ? true
+                                    : false
+                            }
+                            handleView={() => {}}
+                            handleEdit={() => {}}
+                            handleCreate={() => {}}
+                            popupIsVisible={false}
+                        />
+                    );
+                })}
             </div>
         </section>
     );
