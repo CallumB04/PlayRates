d510a85c5f4747d04b3044d64ede9a936fdd6056
Cal <callumburgoyne04@gmail.com>
2025-04-14 01:55:26 +0100

    feat: add game cover, title and release date to page

--- frontend/src/pages/GamePage/GamePage.tsx
+++ frontend/src/pages/GamePage/GamePage.tsx
@@ -21,7 +21,31 @@ const GamePage = () => {
     }, []);
 
     return (
-        <section className="mx-auto mt-20 min-w-[1200px] max-w-[1200px]"></section>
+        <section className="mx-auto mt-20 min-w-[1200px] max-w-[1200px] font-lexend">
+            <span className="flex gap-8">
+                <div className="game-cover h-80">
+                    <img
+                        src={`/PlayRates/assets/game-covers/${game?.id}.png`}
+                        className="size-full rounded-md object-cover"
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-4xl tracking-wide text-text-primary">
+                        {game?.title}
+                    </h2>
+                    <p className="text-xl font-light text-text-secondary">
+                        Released on{" "}
+                        <span className="font-normal">
+                            {game?.releaseDate
+                                ? new Date(game?.releaseDate)
+                                      .toDateString()
+                                      .slice(4)
+                                : "N/A"}
+                        </span>
+                    </p>
+                </div>
+            </span>
+        </section>
     );
 };
 
