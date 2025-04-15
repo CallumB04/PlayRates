0d0d223607f1dfe360a106fdfb6359ee00b1ebce
Cal <callumburgoyne04@gmail.com>
2025-04-15 15:04:52 +0100

    feat: add game platforms

--- frontend/src/pages/GamePage/GamePage.tsx
+++ frontend/src/pages/GamePage/GamePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchGameById, Game } from "../../api";
+import { gamePlatforms } from "../../App";
 
 const GamePage = () => {
     const { gameID } = useParams(); // getting game id from URL
@@ -23,12 +24,14 @@ const GamePage = () => {
     return (
         <section className="mx-auto mt-20 min-w-[1200px] max-w-[1200px] font-lexend">
             <span className="flex gap-8">
+                {/* Left column */}
                 <div className="flex w-full flex-col">
                     <img
                         src={`/PlayRates/assets/game-covers/${game?.id}.png`}
                         className="h-80 max-w-max rounded-md"
                     />
                 </div>
+                {/* Right column */}
                 <div className="flex flex-col gap-2">
                     <h2 className="text-4xl tracking-wide text-text-primary">
                         {game?.title}
@@ -68,6 +71,32 @@ const GamePage = () => {
                         error molestias modi praesentium commodi recusandae
                         consequatur minus.
                     </p>
+                    <span className="mt-2 flex flex-wrap gap-3">
+                        {game?.platforms.map((platform) => {
+                            return (
+                                <span className="flex items-center gap-2 rounded-full border-2 border-text-secondary px-2.5 py-0.5 text-text-primary">
+                                    <p>
+                                        {
+                                            gamePlatforms.find(
+                                                (gamePlatform) =>
+                                                    gamePlatform.name ===
+                                                    platform
+                                            )?.display
+                                        }
+                                    </p>
+                                    <i
+                                        className={
+                                            gamePlatforms.find(
+                                                (gamePlatform) =>
+                                                    gamePlatform.name ===
+                                                    platform
+                                            )?.icon
+                                        }
+                                    ></i>
+                                </span>
+                            );
+                        })}
+                    </span>
                 </div>
             </span>
         </section>
