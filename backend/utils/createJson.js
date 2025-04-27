014af2e8bf468f11ec50cfd2b8097830ddf6c6b4
Cal <callumburgoyne04@gmail.com>
2025-04-27 03:56:54 +0100

    feat: create reviews file path

--- backend/utils/createJson.js
+++ backend/utils/createJson.js
@@ -3,6 +3,7 @@ const usersFilePath = "./data/users.json";
 const gamesFilePath = "./data/games.json";
 const friendsFilePath = "./data/friends.json";
 const gameLogsFilePath = "./data/gamelogs.json";
+const reviewsFilePath = "./data/reviews.json";
 
 // create data folder if doesnt exist
 if (!fs.existsSync("data")) {
@@ -27,4 +28,9 @@ if (!fs.existsSync(friendsFilePath)) {
 // create gamelogs.json with empty object if file doesnt exist
 if (!fs.existsSync(gameLogsFilePath)) {
     fs.writeFileSync(gameLogsFilePath, "{}", "utf8");
+}
+
+// create reviews.json with empty object if file doesnt exist
+if (!fs.existsSync(reviewsFilePath)) {
+    fs.writeFileSync(reviewsFilePath, "{}", "utf8");
 }
